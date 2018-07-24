478a713cb27a7d23047ae90ed1cc10ab77a936f5
Divermv <Divermv@gmail.com>
2018-07-24 21:11:24 +0700

    RAW IP

--- client_proc.c
+++ client_proc.c
@@ -6,13 +6,14 @@ int Raw_client(int port, char *message)
     int sock;
     struct sockaddr_in addr;
     struct UdpHead UDP_Header;
+    struct IPv4Head IP_Header;
     char buf[1024];
     char mes[1016];
     int bytes_read;
     socklen_t addrlen;
     u_short recv_length;
 
-    sock = socket(AF_INET,SOCK_RAW,IPPROTO_UDP);
+    sock = socket(AF_INET,SOCK_RAW,IPPROTO_RAW);
     if(sock < 0)
     {
         perror("socket");
@@ -20,8 +21,22 @@ int Raw_client(int port, char *message)
     }
     addr.sin_family = AF_INET;
    // addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr("192.168.0.8");
+   // addr.sin_addr.s_addr = inet_addr("192.168.0.8");
    // addr.sin_addr.s_addr = inet_addr("8.8.8.8");
+   IP_Header.Ver=0x45;
+   IP_Header.Service=0;
+   IP_Header.len=0;
+   IP_Header.Id=0;
+   IP_Header.offset=0;
+   IP_Header.TTL=128;
+   IP_Header.Next_Prot=17;// UDP
+   IP_Header.CRC=0;
+   IP_Header.sours_IP=inet_addr("192.168.0.8");
+   IP_Header.dest_IP=inet_addr("8.8.8.8");
+
+
+
+
     UDP_Header.dest_port = htons(port);// записываем номер порта получателя
     UDP_Header.src_port = htons(7777);// записываем номер порта отправителя - не принципиален
     for(int i=0 ;i<20;i++)
@@ -31,15 +46,19 @@ int Raw_client(int port, char *message)
 
 	    sprintf(mes,"send message %i",i);// сформировали сообщение
            UDP_Header.length=htons(sizeof(UDP_Header)+strlen(mes));// посчитали размер нагрузки
-	   memcpy((void *)buf,(void *)&UDP_Header,sizeof(UDP_Header));
-           memcpy((void *)(buf+sizeof(UDP_Header)),(void *)mes,strlen(mes));
-	    sendto(sock, buf, sizeof(UDP_Header)+strlen(mes), 0, (struct sockaddr *)&addr, sizeof(addr));
-	    sleep(1);
-         bytes_read = recvfrom(sock, buf, 1024, 0, (struct sockaddr *) &addr, &addrlen);
+	   IP_Header.len=htons(sizeof(UDP_Header)+strlen(mes)+sizeof(IP_Header));// размер для IP
+	   memcpy(buf,&IP_Header,sizeof(IP_Header));
+	   memcpy((void *)(buf+sizeof(IP_Header)),(void *)&UDP_Header,sizeof(UDP_Header));
+           memcpy((void *)(buf+sizeof(IP_Header)+sizeof(UDP_Header)),(void *)mes,strlen(mes));
+	    sendto(sock, buf, sizeof(IP_Header)+sizeof(UDP_Header)+strlen(mes), 0, (struct sockaddr *)&addr, sizeof(addr));
+	    perror("send");
+	    //  sleep(1);
+        // bytes_read = recvfrom(sock, buf, 1024, 0, (struct sockaddr *) &addr, &addrlen);
+
 	 /*получили пакет, нужно проверять его правильность*/
-		/*	 for(int i=0;i<28;i++)
+			 for(int i=0;i<28;i++)
 				 printf("%i=%X ",i,(unsigned char)buf[i]);
-				 printf("\n ");*/
+				 printf("\n ");
 	 if(buf[0]==0x45) // наверное IP
 	     if(buf[9]==0x11)// наверное UDP
 		 if((buf[22]==0x1E) && (buf[23]==0x61))// порт 7777
--- sock_client.h
+++ sock_client.h
@@ -16,4 +16,18 @@ int Raw_client(int port, char *message);
       u_short length;
       u_short checksum;
      };
+    struct IPv4Head
+     {
+	unsigned char Ver;// 1-й байт
+	     unsigned char Service;
+	     u_short       len;
+	     u_short       Id;
+	     u_short       offset;
+	     unsigned char TTL;
+	     unsigned char Next_Prot;
+	     u_short       CRC;
+	     uint32_t      sours_IP;
+	     uint32_t      dest_IP;
+     };
+
 #endif
